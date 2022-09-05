3beec6fe44a60537a9a549e48afb0be2bf39246a
Jean-Luc Van De Velde <jeanlucvdv@gmail.com>
2022-09-05 13:36:32 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Nav from './components/navigation/Nav';
+import './styles/App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/navigation/Nav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className='navi'>
+            <h1>Navigation</h1>
+        </div>
+    );
+};
+
+export default Nav;
