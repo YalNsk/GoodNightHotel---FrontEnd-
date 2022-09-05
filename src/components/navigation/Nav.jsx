45069b9858cf0fb21ad50b06f9a0fe596de8fc28
Jean-Luc Van De Velde <jeanlucvdv@gmail.com>
2022-09-05 15:48:03 +0200

    browserrouter

--- src/components/navigation/Nav.jsx
+++ src/components/navigation/Nav.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from "react";
 
 const Nav = () => {
-    return (
-        <div className='navi'>
-            <h1>Navigation</h1>
-        </div>
-    );
+  return (
+    <div className="navi">
+      <h1>Navigation</h1>
+      <div className="containerButton">
+        <button type="button">S'inscrire</button>
+        <button type="button">Se connecter</button>
+      </div>
+    </div>
+  );
 };
 
 export default Nav;
