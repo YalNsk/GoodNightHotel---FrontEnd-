13a1c43e221f26b1307521f922572a6cf2effbb8
Yaël Nseke-Nzinda <www.yael.nseke@gmail.com>
2022-09-07 14:28:09 +0200

    ?

--- src/pages/search/Search.jsx
+++ src/pages/search/Search.jsx
@@ -7,9 +7,7 @@ const Search = (props) => {
   return (
     <div>
       <Nav />
-<<<<<<< HEAD
       <h1>choicePays</h1>
-=======
       <br />
       <h2>Choisissez votre destination</h2>
       <Pays />
@@ -17,7 +15,6 @@ const Search = (props) => {
       <h2>Affinez votre choix</h2>
       <br />
       <CriteresRecherche />
->>>>>>> 8e7f7688bfddb1b11cfb520f832a46d82a748f05
     </div>
   );
 };
